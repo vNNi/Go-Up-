1da443b23d01dec93abc19e933f9d6618d3b7970
Vinicius Komninakis <komninakis@porquenao.mobi>
2019-04-26 16:35:16 -0300

    taking inputs from user

--- go-up.go
+++ go-up.go
@@ -3,6 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	println("aa")
-	fmt.Println("AAAAAA")
+	welcome()
+}
+func welcome() {
+	var name string
+	var version float32 = 1.1
+	fmt.Print("Say ur name: ")
+	fmt.Scanf("%s", &name)
+	fmt.Println("Welcome", name, "this app is on version:", version)
 }
