a20ce89682f6cb89a09cf5d31aaea853ed0a4ad4
Vinicius Komninakis <komninakis@porquenao.mobi>
2019-04-26 16:56:10 -0300

    add action func

--- go-up.go
+++ go-up.go
@@ -11,4 +11,14 @@ func welcome() {
 	fmt.Print("Say ur name: ")
 	fmt.Scanf("%s", &name)
 	fmt.Println("Welcome", name, "this app is on version:", version)
+	fmt.Println(action())
+}
+func action() int {
+	fmt.Println("--------------------------------------------------")
+	fmt.Println("1- Begin Monitoring")
+	fmt.Println("2- Show Logs")
+	fmt.Println("3- Exit program")
+	var action int
+	fmt.Scanf("%d", &action)
+	return action
 }
